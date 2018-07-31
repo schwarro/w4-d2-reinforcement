8401ebbd8d58735bd5bd160668e6bb0dd7a2c0b2
schwarro <rob.ap.schwartz@gmail.com>
2018-07-31 14:50:03 -0400

    completed exercise

---
+++ collections.rb
@@ -0,0 +1,23 @@
+hash_one = { data: { rooms:
+    [ { id: 1, room_number: "201", capacity: 50}, { id: 2, room_number: "301", capacity: 200 }, { id: 3, room_number: "1B", capacity: 100}
+    ],
+    events: [
+      { id: 1, room_id: 2, start_time: 18, end_time: 20, attendees: 75 },
+      { id: 2, room_id: 1, start_time: 10, end_time: 18, attendees: 25 },
+      { id: 3, room_id: 2, start_time: 10, end_time: 18, attendees: 20 },
+      { id: 4, room_id: 3, start_time: 18, end_time: 21, attendees: 56 },
+    ]
+  }
+}
+
+
+#retrieve capacity of room 201 and store it in a variable
+room_201 = hash_one[:data][:rooms][0][:capacity]
+puts room_201
+
+#find all the events taking place in room 201 and iterate through them if the number of planned atendees will fit
+hash_one[:data][:events].each do |event|
+  if event[:room_id] == 1 && event[:attendees] <= 50
+    puts "Will fit"
+  end
+end
